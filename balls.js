03210b1cbc6cf266ec3be20bfc09db056fbe8325
Darya Khikhol <khikhol.darya@mail.ru>
2024-02-28 20:50:25 +0100

    Update balls.js

--- balls.js
+++ balls.js
@@ -1,3 +1,5 @@
+// 1.Scale riddle. With 8 balls, ex.  [1,2,1,1,1,1,1,1] get position of the “heavy” ball. Indexes are to be chosen at random. Use weights comparison only two times.
+
 function findHeavyBallIndex(balls) {
   const n = balls.length;
 
